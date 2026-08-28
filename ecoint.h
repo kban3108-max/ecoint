61850b6b2c8aff59fa48d91a9b1034d8aea86a8d
kban3108-max <kban3108@gmail.com>
2026-08-28 10:38:47 +0300

    ACTUAL no more copypastas but yeah

--- ecoint.h
+++ ecoint.h
@@ -8,38 +8,36 @@
 #include <stdbool.h>
 
 static inline double getthres(double K, double M, double P, double Pool) {
-	return ((P*P) * K * M) / Pool;
+        return ((P*P) * K * M) / Pool;
 }
 
 static inline double getelo(double K, double M, double P, double Pool, double T, double* Tthres) {
-	*Tthres = getthres(K,M,P,Pool);
-	return (Pool / K / M / P) * T;
+        *Tthres = getthres(K,M,P,Pool);
+        return (Pool / K / M / P) * T;
 }
 
 static inline bool checkthres(double K, double M, double P, double Pool, double* Tthres) {
-	double tmp = (Pool / K / M / P) * *Tthres;
-	return fabs(tmp-P) < 1e-9;
+        double tmp = (Pool / K / M / P) * *Tthres;
+        return fabs(tmp-P) < 1e-9;
 }
 
-static inline int winnings(double* p_a, double* p_b, double* Pool, double* win, double K, double M, double P, double D, int MR, int MP, double* Tthres) {
-	double diff = fabs(*p_a - *p_b);
-	if (win == p_a) {
-		double eb = fmax(MR, *p_b - diff);
-		double loss = fabs(*p_b - eb);
-		*p_b = eb;
-		*Pool = fmax(MP, *Pool - loss);
-		*p_a = *p_a * (1 - D);
-	} else if (win == p_b) {
-		double ea = fmax(MR, *p_a - diff);
-		double loss = fabs(*p_a - ea);
-		*p_a = ea;
-		*Pool = fmax(MP, *Pool - loss);
-		*p_b = *p_b * (1 - D);
-	} else {
-		return ECOINT_NOTGOOD;
-	}
-	*Tthres = getthres(K,M,P,*Pool);
-	return ECOINT_OK;
+static inline int winnings(double* p_a, double* p_b, double* Pool, double* win, double K, double M, double P, double D, double DC, int MR, int MP, double* Tthres) {
+        double diff = fabs(*p_a - *p_b);
+        if (win == p_a) {
+    diff *= 1 - D;
+    *p_a = fmax(MR, *p_a + (diff-(D*DC)));
+    *Pool = fmax(MP, *Pool - (D*DC));
+    *p_b = fmax(MR, *p_b - diff);
+        } else if (win == p_b) {
+    diff *= 1 - D;
+                *p_a = fmax(MR, *p_a - diff);
+                *Pool = fmax(MP, *Pool - (D*DC));
+                *p_b = fmax(MR, *p_b + (diff-(D*DC)));
+        } else {
+                return ECOINT_NOTGOOD;
+        }
+        *Tthres = getthres(K,M,P,*Pool);
+        return ECOINT_OK;
 }
 
 #endif
